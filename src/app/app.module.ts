eaf2b47638c866318f8d65fc137b4f88ab708c38
RSandage <90716032+RSandage@users.noreply.github.com>
2022-12-04 19:55:00 -0600

    Created About

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ImageSliderModule } from './image-slider/image-slider.module';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ServicesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ImageSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SlideInterface } from '../image-slider/types/slide.interface';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  slides: SlideInterface[] = [
+    {url: 'src/assets/images/IMG_0349.jpg', title: "House"},
+    {url: 'src/assets/images/IMG_0229.jpg', title: "Patio"},
+    {url: 'src/assets/images/imagejpeg_0.jpg', title: "Backyard"}
+  ];
 
   constructor() { }
 
