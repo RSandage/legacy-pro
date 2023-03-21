8f0c1c17b9eebb17cb0d3703b7aa28141b7bfe77
RSandage <90716032+RSandage@users.noreply.github.com>
2023-03-21 14:51:32 -0500

    Finished Mobile Formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'legacy-pro';
+
+  scroll(element: HTMLElement) {
+    element.scrollIntoView({behavior: 'smooth'})
+  }
 }
